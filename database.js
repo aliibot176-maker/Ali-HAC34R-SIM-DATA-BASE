0f4257260fb095961f9ba77f63162b88055d068e
aliibot176-maker <aliibot176@gmail.com>
2025-10-23 22:59:11 +0500

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("✅ Kami Flex SIM Database API is working!");
+  res.send("✅ Ali Flex SIM Database API is working!");
 });
 
 app.get("/api/search", async (req, res) => {
